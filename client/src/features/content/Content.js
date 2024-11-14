05b3f5f62991f737d2972e5635f974731eb8ec6b
Josue Martinez <josue.martinez65@yahoo.com>
2024-11-13 22:07:59 -0800

    content resize CSS

--- client/src/features/content/Content.js
+++ client/src/features/content/Content.js
@@ -11,18 +11,14 @@ import {
 import contentList from '../../contentImgs.js'
 
 export function Content() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  // const [incrementAmount, setIncrementAmount] = useState('2');
 
-  const incrementValue = Number(incrementAmount) || 0;
 
   return (
       <div className="contentMain">
-        <h1>Content</h1>
         <div className="content">
           {contentList.map((item) => {
-            return <div className="contentTile" key={item.id}>{item.name}<img className="contentImg" src={item.image}/></div>
+            return <div className="contentTile" key={item.id}><img src={item.image}/></div>
           })}
       </div>
     </div>
