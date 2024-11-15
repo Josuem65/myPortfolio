132cb1302b6749131a9a041c5d962a0d3d6255af
Josue Martinez <josue.martinez65@yahoo.com>
2024-11-15 00:55:28 -0800

    Tiles mobile css

--- client/src/features/tiles/Tiles.js
+++ client/src/features/tiles/Tiles.js
@@ -20,25 +20,10 @@ export function Tiles() {
   }
 
   return (
-    <div>
       <div className="tilesMain">
         {imageList.map((item) => {
           return <div className="tile" onClick={() => openInNewTab(item.url)} key={item.id}>{item.name}<img class="tileImg" src={item.image}/></div>
         })}
       </div>
-    </div>
   );
-}
-
-// buggy
-const handleMouseEnter = (e) => {
-  e.target.style.transform = 'scale(1.2)';
-  e.target.style.zIndex = '1';
-  e.target.style.transition = 'transform 0.4s';
-}
-// buggy
-const handleMouseLeave = (e) => {
-  e.target.style.transform = 'scale(1)';
-  e.target.style.zIndex = '0';
-  e.target.style.transition = 'transform 0.4s';
 }
