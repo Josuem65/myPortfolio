0e828a7d38ed63592c45db4f19804fc419fe40e6
Josue Martinez <josue.martinez65@yahoo.com>
2025-04-23 09:20:43 -0700

    buyMeACoffee functional popup

--- client/src/features/buyMeACoffee/BuyMeACoffee.js
+++ client/src/features/buyMeACoffee/BuyMeACoffee.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react';
+import { isDesktop, isTablet } from 'react-device-detect';
 import BuyMeACoffeeButton from '../../logos/BuyMeACoffeeButton.png';
 
 export const buyMeCoffeeRef = React.createRef();
@@ -10,11 +11,32 @@ export function BuyMeACoffee() {
     buyMeCoffeeRef.current = localBuyMeCoffeeRef.current; // Assign the local reference to the exported reference
   }, []);
 
+  const openWindow = () => {
+    if (isDesktop) {
+      const width = 500;
+      const height = 600;
+      const left = (document.documentElement.clientWidth - width) / 2;
+      const top = (document.documentElement.clientHeight - height) / 2;
+      window.open(
+        "https://www.buymeacoffee.com/josuem95",
+        "popup",
+        `width=${width},height=${height},left=${left},top=${top}`
+      ); 
+        } else if (isTablet) {
+      window.open("https://www.buymeacoffee.com/josuem95", "_blank");
+        } else {
+      window.location.href = "https://www.buymeacoffee.com/josuem95";
+        }
+  }
+
   return (
     <div ref={localBuyMeCoffeeRef} className="buyMeCoffeeSection">
       <div className="coffeeMain">
-        <img src={BuyMeACoffeeButton} alt="BuyMeACoffeeButton" />
+        <img onClick={()=> openWindow()}src={BuyMeACoffeeButton} alt="BuyMeACoffeeButton" />
       </div>
+      <p className="coffeeText">
+        If you like my work and want to support it, consider buying me a coffee! Your support is very appreciated. Thank you!
+      </p>
     </div>
   );
 }
